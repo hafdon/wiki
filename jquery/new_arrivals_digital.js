cbe1a5fce259109710a15220d7516f9d7859af29
jmrlib <45947297+jmrlib@users.noreply.github.com>
2020-12-15 12:32:44 -0500

    Create new_arrivals_digital.js

---
+++ jquery/new_arrivals_digital.js
@@ -0,0 +1,25 @@
+{
+    "queries": [{
+        "target": {
+            "record": {
+                "type": "bib"
+            },
+            "id": 83
+        },
+        "expr": {
+            "op": "greater_than_or_equal",
+            "operands": ["02-01-2017", ""]
+        }
+    }, "and", {
+        "target": {
+            "record": {
+                "type": "bib"
+            },
+            "id": 31
+        },
+        "expr": {
+            "op": "equals",
+            "operands": ["-", ""]
+        }
+    }]
+}
